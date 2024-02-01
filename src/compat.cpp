60b737b313d0f9c0685d6b0ce2431dd09c0ad708
blockmath <bmath2048@gmail.com>
2024-02-01 12:40:32 -0800

    Update compat.cpp

--- src/compat.cpp
+++ src/compat.cpp
@@ -2,7 +2,13 @@
 #include <stdio.h>
 
 int main(int argc, const char *argv[]) {
+  bool issues = false;
   if (sizeof(uint32) != 4) {
     printf("There's a problem with your system: type `uint32` must be a 32-bit unsigned integer; on your system, it's %d bits.\nPlease edit `emul.hpp` and ensure type `uint32` is defined to be a 32-bit unsigned integer.\n", sizeof(uint32)*8);
+    issues = true;
   }
+  if (!issues) {
+    printf("No issues found.\n");
+  }
+  return 0;
 }
